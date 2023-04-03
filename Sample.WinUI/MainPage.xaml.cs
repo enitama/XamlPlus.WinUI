5bbea25fd32cf1fcfe8ba4dd91012dce7188c858
eni <129673786+enitama@users.noreply.github.com>
2023-04-04 01:53:10 +0900

    Partial port of the Sample project to WinUI 3
    Unfortunately ExceptHighContrast doesn't seem to work

---
+++ Sample.WinUI/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace XamlPlusSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ModifyTheme_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var nightStyle = Resources["FbButtonStyle"] as Style;
+            var instaStyle = Resources["InstaButtonStyle"] as Style;
+            var accentStyle = Resources["AccentColorButtonStyle"] as Style;
+            if (button.Style == nightStyle)
+            {
+                button.Style = instaStyle;
+            }
+            else
+            {
+                if (button.Style == instaStyle)
+                {
+                    button.Style = accentStyle;
+                }
+                else
+                {
+                    button.Style = nightStyle;
+                }
+            }
+        }
+
+        private void AddStyle_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Style = Resources["FbButtonStyle"] as Style;
+        }
+
+        private void ResetStyle_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Style = null;
+        }
+    }
+}
